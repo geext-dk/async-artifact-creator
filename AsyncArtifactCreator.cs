7279e5750eec474cc7a2ee43974d3bcf6392f506
Denis Karpovskiy <geext29@gmail.com>
2021-02-07 13:07:53 +0300

    Nest in a new namespace

--- AsyncArtifactCreator.cs
+++ AsyncArtifactCreator.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
-namespace AsyncArtifactCreator
+namespace Geext.AsyncArtifactCreator
 {
     public abstract class AsyncArtifactCreator<TArtifactRequest, TArtifact, TArtifactIdentity>
     {
